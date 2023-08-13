ad5a51836c9f594be747cb6b5d1ac2bbd823dd14
Govind Bajaj <meetgovindbajaj@gmail.com>
2023-08-14 02:31:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+  let arr = document
+    .getElementById("rope-lengths")
+    .value.split(/,\s*/)
+    .map((val) => parseInt(val));
+  let ans = 0;
+  while (arr.length > 1) {
+    arr.sort((a, b) => a - b);
+    ans += arr[0] + arr[1];
+    arr[1] += arr[0];
+    arr.shift();
+  }
+  document.getElementById("result").innerText = ans;
+}
